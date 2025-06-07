f042162cdd66e2ad5a070431ab15b1333e422f73
Lydia Gao <l75gao@uwaterloo.ca>
2025-06-06 23:36:45 -0400

    add steps page

---
+++ steps/src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) {
+      // don't change step manually like step = step - 1, because react is all about immutability and functional state updates.
+      // react don't really mutate the old state, but created a new one, so react can diff the old and new state, so that it can and only update the parts that changed to optimize performance.
+      // so, always tell react when and how you want to change the state by calling the state setter function, and react will take care of the rest behind the scenes, like re-render
+      // on developer side, always treat state as immutable, and never mutate it directly.
+      setStep(step - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < messages.length) {
+      setStep(step + 1);
+    }
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
