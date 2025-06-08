d969d96b4fd8be6b0a3a2e92feae5f3b681784b0
Lydia Gao <l75gao@uwaterloo.ca>
2025-06-08 16:34:15 -0400

    build star rating api

--- usepopcorn/src/StarRating.js
+++ usepopcorn/src/StarRating.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
+// outside const will not be re-rendered
 const containerStyle = {
   display: "flex",
   alignItems: "center",
-  gap: "16px",
+  gap: "0.5rem",
 };
 
 const starContainerStyle = {
@@ -13,11 +14,11 @@ const starContainerStyle = {
 
 StarRating.propTypes = {
   maxRating: PropTypes.number,
-  defaultRating: PropTypes.number,
   color: PropTypes.string,
   size: PropTypes.number,
-  messages: PropTypes.array,
   className: PropTypes.string,
+  messages: PropTypes.array,
+  defaultRating: PropTypes.number,
   onSetRating: PropTypes.func,
 };
 
@@ -28,31 +29,31 @@ export default function StarRating({
   className = "",
   messages = [],
   defaultRating = 0,
-  onSetRating,
+  onSetRating = () => {},
 }) {
   const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
-  function handleRating(rating) {
-    setRating(rating);
-    onSetRating(rating);
-  }
-
   const textStyle = {
     lineHeight: "1",
-    margin: "0",
+    margin: 0,
     color,
-    fontSize: `${size / 1.5}px`,
+    fontSize: `${size / 2}px`,
   };
+
+  function handleRate(rating) {
+    onSetRating(rating);
+    setRating(rating);
+  }
 
   return (
     <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
-        {Array.from({ length: maxRating }, (_, i) => (
+        {Array.from({ length: maxRating }).map((_, i) => (
           <Star
             key={i}
-            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-            onRate={() => handleRating(i + 1)}
+            onRate={() => handleRate(i + 1)}
+            full={i < (tempRating || rating)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
             color={color}
@@ -76,7 +77,6 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
     display: "block",
     cursor: "pointer",
   };
-
   return (
     <span
       role="button"
@@ -130,6 +130,18 @@ FULL STAR
 
 EMPTY STAR
 
-
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="#000"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="{2}"
+    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+  />
+</svg>
 
 */
