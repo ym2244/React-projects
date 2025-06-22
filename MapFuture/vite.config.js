7cc29a605b01f48b02b4392de7427b727c54a218
Lydia Gao <l75gao@uwaterloo.ca>
2025-06-22 13:10:11 -0400

    feat: Add Docker configuration and setup for frontend, backend, and fake API services

--- MapFuture/vite.config.js
+++ MapFuture/vite.config.js
@@ -4,5 +4,9 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
-});
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  
+    port: 5173,
+  }
+})
